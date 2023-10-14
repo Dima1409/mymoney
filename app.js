4e59dd6a9e2ee67e6eaf1d9a6315ab3e0673bee5
Dima <57713733+Dima1409@users.noreply.github.com>
2023-10-14 22:08:57 +0300

    i nodemon

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { routerCash, routerWallets } = require("./routes/api");
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
-
+            
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
